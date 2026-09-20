639da790f63cc0f20d3fb2df3b1e6fcd26094e17
Eduardo Maia <dev.eduardomaia@gmail.com>
2026-09-20 11:21:21 -0300

    Find product with highest sales value

---
+++ 07.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+dados = {
+    "produto": ["Celular", "Tablet", "Fone", "Cabo", "Notebook"],
+    "preco": [2000, 1200, 300, 50, 3500],
+    "quantidade": [3, 2, 5, 10, 2]
+}
+
+df = pd.DataFrame(dados)
+
+df["valor_venda"] = df["preco"] * df["quantidade"]
+
+index_max = df["valor_venda"].idxmax() 
+
+print(df["produto"][index_max])
+
