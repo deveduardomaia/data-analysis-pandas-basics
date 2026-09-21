18d89104ecbe3c25e07f778d533e26e00bf42fa0
Eduardo Maia <dev.eduardomaia@gmail.com>
2026-09-21 12:50:53 -0300

    Count products above price threshold

---
+++ 13.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+dados = {
+    "produto": ["Celular", "Tablet", "Fone", "Cabo", "Notebook"],
+    "preco": [2000, 1200, 300, 50, 3500],
+    "quantidade": [3, 2, 5, 10, 2]
+}
+
+df = pd.DataFrame(dados)
+
+print(df["preco"][df["preco"]>1000] .count())
+
+
+#print(df[df["preco"]>100]["preco"].count())
