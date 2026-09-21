3e1a94c46a6e9670dab7ebb5bf7e966e66edb458
Eduardo Maia <dev.eduardomaia@gmail.com>
2026-09-21 12:20:42 -0300

    Calculate average price of high-value products

---
+++ 12.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+dados = {
+    "produto": ["Celular", "Tablet", "Fone", "Cabo", "Notebook"],
+    "preco": [2000, 1200, 300, 50, 3500],
+    "quantidade": [3, 2, 5, 10, 2]
+}
+
+df = pd.DataFrame(dados)
+
+print(df[df["preco"] > 1000]["preco"].mean())
